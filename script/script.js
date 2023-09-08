358ded99482a84f04c001b7b9587a55527b8b9c2
VikVanDik <95vlc17@gmail.com>
2023-09-08 16:45:29 +0200

    [add] : js

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// raccogliere i dati dall'utente
+// calcolare prezzo ed eventuali sconti
+// creare comparsa della card biglietto alla pressione del bottone
+// creare reset ed eliminazione della card alla pressione del bottone reset
+
+const km = document.getElementById('km').value
+
+console.log(km);
+
+
