ec76ca0a8d64b206cfc61a099f557bca9ed0bb1d
VikVanDik <95vlc17@gmail.com>
2023-09-10 15:59:19 +0200

    [add]: final fixes to logic and reload button

--- script/script.js
+++ script/script.js
@@ -17,32 +17,34 @@ let cartNumber = document.getElementById('cart')
 let PD = document.getElementById('PD')
 let cost = document.getElementById('cost')
 
+
 send.addEventListener('click', function(){
-  disappear.classList.remove('d-none');
+  
   const km = parseInt(document.getElementById('km').value)
   const name = document.getElementById('name').value
   let age = parseInt(document.getElementById('age').value)
   console.log(age)
   let price = km * priceMulti
+  disappear.classList.remove('d-none');
   if (age == 1) {
     let priceUA = price * multiUA
     let ticketUA = `Biglietto Minorenni`
     ticketType.innerHTML = ticketUA;
-    cost.innerHTML = priceUA
+    cost.innerHTML = `${priceUA.toFixed(2)}€`
     console.log(ticketUA)
     console.log(priceUA)
   } else if (age == 2) {
     let priceN = price
     let ticketN = `Biglietto Standard`
     ticketType.innerHTML = ticketN;
-    cost.innerHTML = priceN
+    cost.innerHTML = `${priceN.toFixed(2)}€`
     console.log(ticketN)
     console.log(priceN)
   } else if (age == 3) {
     let priceA = price * multiA 
     let ticketA = `Biglietto Silver`
     ticketType.innerHTML = ticketA;
-    cost.innerHTML = priceA
+    cost.innerHTML = `${priceA.toFixed(2)}€`
     console.log(ticketA)
     console.log(priceA)
   }
@@ -50,5 +52,4 @@ send.addEventListener('click', function(){
   messageName.innerHTML=name;
   cartNumber.innerHTML = Math.floor(Math.random() * 9) + 1;
   PD.innerHTML = Math.floor(Math.random() * (9999 - 9000)) + 9000;
-
 })
